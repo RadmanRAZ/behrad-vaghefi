e5bd63adda350d834d8b51fb37396805d2765cd9
radman <radmana92@gmail.com>
2022-05-04 21:33:04 +0430

    updating

---
+++ js/mytype.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    var typed = new Typed('#typed', {
+        strings: [
+            "Designer",
+            "Architecture",
+            "Photographer",
+            "CG Artist"
+        ],
+        loop: true,
+        typeSpeed: 50,
+        backSpeed: 50
+    });
+})
