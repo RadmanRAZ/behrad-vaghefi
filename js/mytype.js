d03eca20420f29d08ddad0b35a86ac01b26055c1
radman <radmana92@gmail.com>
2022-05-15 21:23:36 +0430

    compressing code

--- js/mytype.js
+++ js/mytype.js
@@ -1,13 +1,1 @@
-$(document).ready(function() {
-    var typed = new Typed('#typed', {
-        strings: [
-            "Designer",
-            "Architecture",
-            "Photographer",
-            "CG Artist"
-        ],
-        loop: true,
-        typeSpeed: 50,
-        backSpeed: 50
-    });
-})
+$(document).ready(function(){var a=new Typed("#typed",{strings:["Designer","Architecture","Photographer","CG Artist"],loop:true,typeSpeed:50,backSpeed:50})});
