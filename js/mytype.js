2e765f785983d1cc608db7cf1e3d47178aeadb74
RadmanRAZ <radmana92@gmail.com>
2024-08-19 22:13:39 +0330

    fixed typed.js

--- js/mytype.js
+++ js/mytype.js
@@ -1,1 +1,9 @@
-$(document).ready(function(){var a=new Typed("#typed",{strings:["Designer","Architecture","Photographer","CG Artist"],loop:true,typeSpeed:50,backSpeed:50})});
+
+var typed = new Typed('#typed', {
+    strings:[" Designer"," Architecture"," Photographer"," CG Artist"],
+    typeSpeed: 50,
+    backSpeed : 50,
+    loop : true
+  });
+
+// var a=new Typed("#typed",{strings:["Designer","Architecture","Photographer","CG Artist"],loop:true,typeSpeed:50,backSpeed:50
